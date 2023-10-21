8a96b07701242e8cf2f3fdf5eecec2abe5de6da7
nghiant <nghiant9608@gmail.com>
2023-10-21 10:35:36 +0700

    find by slug

--- be/src/api/product/controllers/product.js
+++ be/src/api/product/controllers/product.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+    // Query by slug
+    async findOne(ctx) {
+        // thanks to the custom route we have now a slug variable
+        // instead of the default id
+        const slug = ctx.params?.id;
+        console.log(slug);
+        const entity = await strapi.db.query('api::product.product').findOne({
+            where: { slug }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    },
+}));
